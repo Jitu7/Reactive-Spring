b00621fb126ca0410ebf3ad4703dc1a68c63a757
Jyotirmaya <jyotirmaya12@gmail.com>
2022-08-24 18:35:53 +0530

    Revert "Reactive First Application With DB"
    This reverts commit 36bdd0f76c8c0483436b9850c194139112a2d537.

--- reactive-1/src/main/java/com/example/reactive/model/Product.java
+++
@@ -1,6 +0,0 @@
-package com.example.reactive.model;
-
-import org.springframework.data.annotation.Id;
-
-public record Product(@Id int id, String name) {
-}
--- reactive-1/src/main/java/com/example/reactive/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.reactive.repositories;
-
-import com.example.reactive.model.Product;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface ProductRepository extends ReactiveCrudRepository<Product, Integer> {
-}
